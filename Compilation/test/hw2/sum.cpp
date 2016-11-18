461499686169ef64dfbdeee84f586481112b7250
Json Lee <lijiansong@ict.ac.cn>
2016-11-18 19:20:10 +0800

    fucking error!  a shit fly prevent me from writing Makefile
    fucking error!  a shit fly prevent me from writing my independent Makefile,now it works

---
+++ Compilation/test/hw2/sum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+using namespace std;
+int sum(int a,int b)
+{
+	return a+b;
+}
+int main(int argc, char *argv[])
+{
+	printf("%d\n",sum(1,2));
+	return 0;
+}
