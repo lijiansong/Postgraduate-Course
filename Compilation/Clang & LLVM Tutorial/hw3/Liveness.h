1ce0a26c09632d4181973740e4c709ebea51b8aa
Json Lee <lijiansong@ict.ac.cn>
2016-12-25 00:03:56 +0800

    so much trouble

--- Compilation/Clang & LLVM Tutorial/hw3/Liveness.h
+++ Compilation/Clang & LLVM Tutorial/hw3/Liveness.h
@@ -22,15 +22,20 @@
 #include <vector>
 using namespace llvm;
 using namespace std;
-
+#define MINUS_INF -0x7ffffff
+#define INF 0x7ffffff
+//typedef pair<>
+typedef pair< map<string,vector<int> >,map<string,vector<int> > > true_false_out_pair;
 struct LivenessInfo 
 {
    std::set<Instruction *> LiveVars;/// Set of variables which are live
-   
-   // interval of each vars,name of each vars is the key, and vector<int> is its range 
-   map<string,pair<int,int> > VarRanges;
 
-   LivenessInfo() : LiveVars() {}
+   // interval of each vars,name of each vars is the key, and vector<int> is its range 
+   map<string,vector<int> > VarRanges;
+   map<string,vector<int> > TrueOut;
+   map<string,vector<int> > FalseOut;
+
+   LivenessInfo() : LiveVars(),VarRanges(),TrueOut(),FalseOut() {}
    LivenessInfo(const LivenessInfo & info) : LiveVars(info.LiveVars) {}
   
    bool operator == (const LivenessInfo & info) const 
@@ -54,36 +59,328 @@ inline raw_ostream &operator<<(raw_ostream &out, const LivenessInfo &info)
 class LivenessVisitor : public DataflowVisitor<struct LivenessInfo> 
 {
 public:
-   LivenessVisitor() {}
+    LivenessVisitor() {}
 
-   void merge(LivenessInfo * dest, const LivenessInfo & src) override 
-   {
-       for (std::set<Instruction *>::const_iterator ii = src.LiveVars.begin(), 
-            ie = src.LiveVars.end(); ii != ie; ++ii) 
-       {
-           //errs()<<"+++++ "<<*ii<<"\n";
+    void merge(LivenessInfo * dest, const LivenessInfo & src) override 
+    {
+         // for (std::set<Instruction *>::const_iterator ii = src.LiveVars.begin(), 
+         //      ie = src.LiveVars.end(); ii != ie; ++ii) 
+         // {
+         //     //errs()<<"+++++ "<<*ii<<"\n";
 
-           dest->LiveVars.insert(*ii);
-           // if(isa<ICmpInst>(ii))
-           // {
-           //  //get its condition val and update its range 
-           // }
-       }
-   }
-    //handle the icmpinst of the bb's pred basic block
-    void handlePredIcmp(/*const*/ BasicBlock &bb) override
+         //     dest->LiveVars.insert(*ii);
+         //     // if(isa<ICmpInst>(ii))
+         //     // {
+         //     //  //get its condition val and update its range 
+         //     // }
+         // }
+    }
+
+    ConstantInt * tryGetConstantValue(Value * value) 
+    {
+        ConstantInt * CI = dyn_cast<ConstantInt>(value);
+        if (CI && CI->getBitWidth() <= 32) 
+        {
+            return CI;
+        }
+        return NULL;
+    }
+
+    vector<int> tryGetRange(Value * value,LivenessInfo *dfval) 
+    {
+        map<string,vector<int> > varRanges = dfval->VarRanges;
+        if( varRanges.count(value->getName().str()) > 0 )
+        return varRanges[value->getName().str()];
+        vector<int> empty;
+        // //assign [-inf,inf]
+        // empty.push_back(MINUS_INF);
+        // empty.push_back(INF);
+        return empty;
+    }
+
+    //intersect operation
+    vector<int> intersect(vector<int> left, vector<int> right)
+    {
+        vector<int> result;
+        if (left.empty())
+            result = left;
+        else if (right.empty())
+            result = right;
+        else
+        {
+            int varmin = max(left[0], right[0]);
+            int varmax = min(left[1], right[1]);
+            if (varmin <= varmax) 
+            { 
+                result.push_back(varmin);
+                result.push_back(varmax);
+            }
+        }
+        return result;
+    }
+
+    true_false_out_pair getTrueFalseOut(BranchInst *inst)
+    {
+        map<string,vector<int> > trueOut;
+        map<string,vector<int> > falseOut;
+
+        Value *condition = inst->getCondition();
+        //ICmpInst *cmpInst = (ICmpInst *)condition;
+        ICmpInst *cmpInst=dyn_cast<ICmpInst>(condition);
+
+        int predicate = cmpInst->isSigned() ? cmpInst->getSignedPredicate() : cmpInst->getUnsignedPredicate();
+        //get the lhs and the rhs
+        Value * lhs = cmpInst->getOperand(0);
+        Value * rhs = cmpInst->getOperand(1);
+        ConstantInt * rhsConstant = tryGetConstantValue(rhs);
+        ConstantInt * lhsConstant = tryGetConstantValue(lhs);
+        vector<int> lhsRange = tryGetRange(lhs,(*result)[pred_bb].second);
+        vector<int> rhsRange = tryGetRange(rhs,(*result)[pred_bb].second);
+
+        vector<int> range; //empty
+
+        Value * variable = NULL;
+        ConstantInt * constant = NULL;
+
+        if((lhsConstant && rhsConstant) || (lhsRange.empty() && rhsRange.empty()))//both lhs and rhs is constant
+        {
+            errs()<<"both lhs and rhs is constant!"<<"\n";
+            return;
+        }
+        //rhs is a constant int, but lhs is not
+        else if(rhsConstant)
+        {
+            variable=lhs;
+            constant=rhsConstant;
+        }
+        //lhs is a constant int, but rhs is not
+        else if(lhsConstant)
+        {
+            variable=rhs;
+            constant=lhsConstant;
+        }
+        //both variables
+        else
+        {
+            //both have ranges, we have to assign new may ranges to both variables!
+            if (!lhsRange.empty() && !rhsRange.empty()) 
+            {
+                variable = NULL; 
+            }
+            // assign to rhs, since it has no range
+            else if(!lhsRange.empty())
+            {
+                range=lhsRange;
+                variable=rhs;
+            }
+            //assign to lhs, since it has no range
+            else if(!rhsRange.empty())
+            {
+                range=rhsRange;
+                variable=lhs;
+            }
+        }//end of else
+
+        BasicBlock * trueBlock = branchInst->getSuccessor(0);
+        BasicBlock * falseBlock = branchInst->getSuccessor(1);
+        //process each probably case
+        if(variable && constant)
+        {
+            vector<int> variableRange = tryGetRange(variable,(*result)[pred_bb].second);
+            // X == C
+            if(predicate == CmpInst::ICMP_EQ)
+            {
+                vector<int> constantRange;
+                map<string,vector<int>> trueMap=&((*result)[pred_bb].second).VarRanges;
+                if(trueMap.count(variable->getName().str())>0)
+                {
+                    trueMap.erase(variable->getName().str());
+                    constantRange.push_back(constant->getSExtValue());
+                    constantRange.push_back(constant->getSExtValue());
+                    vector<int> intersection = intersect(variableRange, constantRange);
+                    if (!intersection.empty()) 
+                    {
+                        trueMap[variable->getName().str()] = intersection;
+                    }
+                }
+                else
+                {
+                }
+            }
+            // X != C
+            else if(predicate == CmpInst::ICMP_NE)
+            {
+            }
+            // X > C
+            else if( ( (predicate == CmpInst::ICMP_UGT || predicate == CmpInst::ICMP_SGT) && variable == lhs ) 
+                      || ( (predicate == CmpInst::ICMP_ULT || predicate == CmpInst::ICMP_SLT) && variable == rhs ) )
+            {
+            }
+            // X < C
+            else if( ( (predicate == CmpInst::ICMP_ULT || predicate == CmpInst::ICMP_SLT) && variable == lhs ) 
+                      || ( (predicate == CmpInst::ICMP_UGT || predicate == CmpInst::ICMP_SGT) && variable == rhs ) )
+            {
+            }
+            // X >= C
+            else if( ( (predicate == CmpInst::ICMP_UGE || predicate == CmpInst::ICMP_SGE) && variable == lhs ) 
+                      || ( (predicate == CmpInst::ICMP_ULE || predicate == CmpInst::ICMP_SLE) && variable == rhs ) )
+            {
+            }
+            // X <= C
+            else if( ( (predicate == CmpInst::ICMP_ULE || predicate == CmpInst::ICMP_SLE) && variable == lhs ) 
+                      || ( (predicate == CmpInst::ICMP_UGE || predicate == CmpInst::ICMP_SGE) && variable == rhs ) )
+            {
+            }
+
+            else if (variable && !range.empty())
+            {
+            }
+            //both operands are ranges
+            else
+            {
+            }
+        }//end of if variable && constant
+        return make_pair<trueOut,falseOut>;
+    }
+    //handle the icmpinst of the pred_bb's pred basic block
+    void handlePredIcmp(/*const*/ BasicBlock *pred_bb,
+                                  /*BasicBlock *bb,*/
+                                  typename DataflowResult<LivenessInfo>::Type &result) override
     {
        //for each intruction in the basic block
-       BasicBlock::iterator ii = bb->begin (), ie = bb->end ();
+       BasicBlock::iterator ii = pred_bb->begin (), ie = pred_bb->end ();
        for (; ii != ie; ++ii)
        {
-         Instruction *inst = dyn_cast < Instruction > (ii);
-         if (isa < ICmpInst > (inst))
+         Instruction *inst = dyn_cast < Instruction > (*ii);
+
+         if (isa < ICmpInst > (inst) )
          {
+          Instruction *next_inst=dyn_cast<Instruction>(*(ii+1));
           //get its condition vars and the lhs and rhs
-         }
-         if(isa<BranchInst>)
-       }
+          //branchInst->isConditional()
+          if(isa<BranchInst>(next_inst))
+          {
+              ICmpInst *cmpInst=dyn_cast<ICmpInst>(inst);
+              BranchInst *branchInst=dyn_cast<BranchInst>(next_inst);
+              int predicate = cmpInst->isSigned() ? cmpInst->getSignedPredicate() : cmpInst->getUnsignedPredicate();
+              
+              //get the lhs and the rhs
+              Value * lhs = cmpInst->getOperand(0);
+              Value * rhs = cmpInst->getOperand(1);
+              ConstantInt * rhsConstant = tryGetConstantValue(rhs);
+              ConstantInt * lhsConstant = tryGetConstantValue(lhs);
+              vector<int> lhsRange = tryGetRange(lhs,(*result)[pred_bb].second);
+              vector<int> rhsRange = tryGetRange(rhs,(*result)[pred_bb].second);
+
+              vector<int> range; //empty
+
+              Value * variable = NULL;
+              ConstantInt * constant = NULL;
+
+              if((lhsConstant && rhsConstant) || (lhsRange.empty() && rhsRange.empty()))//both lhs and rhs is constant
+              {
+                  errs()<<"both lhs and rhs is constant!"<<"\n";
+                  return;
+              }
+              //rhs is a constant int, but lhs is not
+              else if(rhsConstant)
+              {
+                  variable=lhs;
+                  constant=rhsConstant;
+              }
+              //lhs is a constant int, but rhs is not
+              else if(lhsConstant)
+              {
+                  variable=rhs;
+                  constant=lhsConstant;
+              }
+              //both variables
+              else
+              {
+                  //both have ranges, we have to assign new may ranges to both variables!
+                  if (!lhsRange.empty() && !rhsRange.empty()) 
+                  {
+                      variable = NULL; 
+                  }
+                  // assign to rhs, since it has no range
+                  else if(!lhsRange.empty())
+                  {
+                      range=lhsRange;
+                      variable=rhs;
+                  }
+                  //assign to lhs, since it has no range
+                  else if(!rhsRange.empty())
+                  {
+                      range=rhsRange;
+                      variable=lhs;
+                  }
+              }
+
+              BasicBlock * trueBlock = branchInst->getSuccessor(0);
+              BasicBlock * falseBlock = branchInst->getSuccessor(1);
+              //process each probably case
+              if(variable && constant)
+              {
+                  vector<int> variableRange = tryGetRange(variable,(*result)[pred_bb].second);
+                  // X == C
+                  if(predicate == CmpInst::ICMP_EQ)
+                  {
+                      vector<int> constantRange;
+                      map<string,vector<int>> trueMap=&((*result)[pred_bb].second).VarRanges;
+                      if(trueMap.count(variable->getName().str())>0)
+                      {
+                          trueMap.erase(variable->getName().str());
+                          constantRange.push_back(constant->getSExtValue());
+                          constantRange.push_back(constant->getSExtValue());
+                          vector<int> intersection = intersect(variableRange, constantRange);
+                          if (!intersection.empty()) 
+                          {
+                              trueMap[variable->getName().str()] = intersection;
+                          }
+                      }
+                      else
+                      {
+
+                      }
+                  }
+                  // X != C
+                  else if(predicate == CmpInst::ICMP_NE)
+                  {
+                  }
+                  // X > C
+                  else if( ( (predicate == CmpInst::ICMP_UGT || predicate == CmpInst::ICMP_SGT) && variable == lhs ) 
+                            || ( (predicate == CmpInst::ICMP_ULT || predicate == CmpInst::ICMP_SLT) && variable == rhs ) )
+                  {
+                  }
+                  // X < C
+                  else if( ( (predicate == CmpInst::ICMP_ULT || predicate == CmpInst::ICMP_SLT) && variable == lhs ) 
+                            || ( (predicate == CmpInst::ICMP_UGT || predicate == CmpInst::ICMP_SGT) && variable == rhs ) )
+                  {
+                  }
+                  // X >= C
+                  else if( ( (predicate == CmpInst::ICMP_UGE || predicate == CmpInst::ICMP_SGE) && variable == lhs ) 
+                            || ( (predicate == CmpInst::ICMP_ULE || predicate == CmpInst::ICMP_SLE) && variable == rhs ) )
+                  {
+                  }
+                  // X <= C
+                  else if( ( (predicate == CmpInst::ICMP_ULE || predicate == CmpInst::ICMP_SLE) && variable == lhs ) 
+                            || ( (predicate == CmpInst::ICMP_UGE || predicate == CmpInst::ICMP_SGE) && variable == rhs ) )
+                  {
+                  }
+
+                  else if (variable && !range.empty())
+                  {
+                  }
+                  //both operands are ranges
+                  else
+                  {
+                  }
+
+
+              }
+          }//end of if isa<BranchInst>(next_inst)
+         }//end of if isa < ICmpInst > (inst)
+       }//end of for
     }
 
     //handle binary op,e.g.: + - * /
@@ -124,6 +421,22 @@ public:
    void compDFVal(Instruction *inst, LivenessInfo * dfval) override
    {
         if (isa<DbgInfoIntrinsic>(inst)) return;
+        // if(isa<ICmpInst>(inst))
+        // {
+        //   ICmpInst *icmpInst= dyn_cast<ICmpInst>(inst);
+
+        // }
+        if(isa<BranchInst>(inst))
+        {
+          BranchInst *branch=dyn_cast<BranchInst>(inst);
+          if(branch->isConditional())
+          {
+            Value * condition = branch->getCondition();
+            ICmpInst * cmpInst = (ICmpInst *)condition;
+
+            //handleConditionalBranch();
+          }
+        }
         dfval->LiveVars.erase(inst);
         for(User::op_iterator oi = inst->op_begin(), oe = inst->op_end();
             oi != oe; ++oi) 
@@ -135,8 +448,7 @@ public:
             errs()<<*val<<"\n";
 
            }
-           // else if(isa<>(val))
-           // {}
+           
         }
    }
 };
